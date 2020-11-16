8395260dc899ac4650a7bee28b36a327bdaf9ea6
JamesRileydev <jamesriley.dev@gmail.com>
2020-11-16 15:14:10 -0500

    Added searchform and content-excerpt files

--- wp-content/themes/udemy/includes/widgets.php
+++ wp-content/themes/udemy/includes/widgets.php
@@ -4,6 +4,10 @@ function ju_widgets(){
     register_sidebar([
         'name'          => __( 'My First Theme Sidebar', 'udemy' ),
         'id'            => 'ju_sidebar',
-        'description'   => __( 'Sidebar for the theme Udemy', 'udemey' )
+        'description'   => __( 'Sidebar for the theme Udemy', 'udemey' ),
+        'before_widget' => '<div id="%1$s" class="widget clearfix %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>'
     ]);
 }
